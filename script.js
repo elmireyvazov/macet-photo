080e6d9fad85bff54e7a8ce3af0c3fd47403feca
Elmir <elmireyvazov11@gmail.com>
2023-02-09 23:14:21 +0300

    сделать модальное окно

--- script.js
+++ script.js
@@ -11,3 +11,37 @@ closePopUp.addEventListener('click', () => {
     popUp.classList.remove('active');
 })
 
+const form = document.getElementById('form');
+form.addEventListener('submit', getFormValue);
+
+function getFormValue(event) {
+    event.preventDefault();
+
+    data = form.querySelector('[name="data"]'), 
+    email = form.querySelector('[name="email"]'), 
+    nam = form.querySelector('[name="nams"]'), 
+    plan = form.querySelector('[name="surname"]'); 
+    telephone = form.querySelector('[name="telephone"]'); 
+    cards = form.querySelector('[name="cards"]');
+    data__cards = form.querySelector('[name="data__cards"]');
+    cvv__cards = form.querySelector('[name="cvv__cards"]');
+    terms = form.querySelector('[name="terms"]')
+}
+
+    const data = {
+        data: data.value,
+        email: email.value,
+        nams: nams.value,
+        surname: surname.value,
+        telephone: telephone.value,
+        cards: cards.value,
+        data__cards: data__cards.value,
+        cvv__cards: cvv__cards.value,
+        terms: terms.value
+    };
+
+    console.log(data);
+
+
+
+
